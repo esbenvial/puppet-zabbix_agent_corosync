73810e9ad54b80122577c5f9b96b44a6c62025b1
Esben Vial <cjs614@hum.ku.dk>
2018-03-16 15:01:38 +0100

    correct module name

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,19 +5,19 @@ class zabbix_agent_corosync (
 
   file { 'pacemaker.conf':
     path   => "${agent_include}/pacemaker.conf",
-    source => 'puppet:///modules/zabbix_agent_pacemaker/zabbix-agent-pacemaker.conf',
+    source => 'puppet:///modules/${module_name}/zabbix-agent-pacemaker.conf',
     notify => Service['zabbix-agent'],
   }
 
   file { 'check_pacemaker_actions':
     path   => "${bin_path}/check_pacemaker_actions",
-    source => 'puppet:///modules/zabbix_agent_pacemaker/check_pacemaker_actions',
+    source => 'puppet:///modules/${module_name}/check_pacemaker_actions',
     mode   => '0775',
   }
 
   file { 'crm_mon_stats.sh':
     path   => "${bin_path}/crm_mon_stats.sh",
-    source => 'puppet:///modules/zabbix_agent_pacemaker/crm_mon_stats.sh',
+    source => 'puppet:///modules/${module_name}/crm_mon_stats.sh',
     mode   => '0775',
   }
 
