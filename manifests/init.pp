2b4cd9dd78b6c4cfb7fbaef3b5dcce2e56bc0c99
Esben Vial <cjs614@hum.ku.dk>
2018-03-16 14:17:29 +0100

    first commit

---
+++ manifests/init.pp
@@ -0,0 +1,29 @@
+class zabbix_agent_pacemaker (
+  $agent_include  = '/etc/zabbix/zabbix_agentd.d/',
+  $bin_path       = '/usr/local/sbin',
+  ) {
+
+  file { 'pacemaker.conf':
+    path    => "$agent_include/pacemaker.conf",
+    source  => 'puppet:///modules/zabbix_agent_pacemaker/zabbix-agent-pacemaker.conf',
+    notify  => Service['zabbix-agent'],
+  }
+
+  file { 'check_pacemaker_actions':
+    path    => "$bin_path/check_pacemaker_actions",
+    source  => 'puppet:///modules/zabbix_agent_pacemaker/check_pacemaker_actions',
+    mode    => '0775',
+  }
+
+  file { 'crm_mon_stats.sh':
+    path    => "$bin_path/crm_mon_stats.sh",
+    source  => 'puppet:///modules/zabbix_agent_pacemaker/crm_mon_stats.sh',
+    mode    => '0775',
+  }
+
+  exec {"zabbix haclient membership":
+    unless => "grep -q 'haclient\\S*zabbix' /etc/group",
+    command => "usermod -aG haclient zabbix",
+    notify  => Service['zabbix-agent'],
+  }
+}
