78f6e8089607bddb226b5aa176cfa074d7ce1231
Esben Vial <cjs614@hum.ku.dk>
2018-03-16 15:03:51 +0100

    fix paths

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,25 +5,25 @@ class zabbix_agent_corosync (
 
   file { 'pacemaker.conf':
     path   => "${agent_include}/pacemaker.conf",
-    source => 'puppet:///modules/${module_name}/zabbix-agent-pacemaker.conf',
+    source => "puppet:///modules/${module_name}/zabbix-agent-pacemaker.conf",
     notify => Service['zabbix-agent'],
   }
 
   file { 'check_pacemaker_actions':
     path   => "${bin_path}/check_pacemaker_actions",
-    source => 'puppet:///modules/${module_name}/check_pacemaker_actions',
+    source => "puppet:///modules/${module_name}/check_pacemaker_actions",
     mode   => '0775',
   }
 
   file { 'crm_mon_stats.sh':
     path   => "${bin_path}/crm_mon_stats.sh",
-    source => 'puppet:///modules/${module_name}/crm_mon_stats.sh',
+    source => "puppet:///modules/${module_name}/crm_mon_stats.sh",
     mode   => '0775',
   }
 
   exec {'zabbix haclient membership':
     unless  => "/bin/grep -q 'hacluster\\S*zabbix' /etc/group",
-    command => '/sbin/usermod -aG hacluster zabbix',
+    command => '/usr/sbin/usermod -aG hacluster zabbix',
     notify  => Service['zabbix-agent'],
   }
 }
